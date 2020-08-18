807c9bc9675642cbfe81ce90e67c212b6fe8c657
Manvendra27 <60910666+Manvendra27@users.noreply.github.com>
2020-08-18 23:55:39 +0530

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const app = express();
 const fs = require('fs');
 const path = require('path');
@@ -10,9 +10,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static("."));
 
-// app.get('/style.css', function(req, res) {
-//     res.sendFile(__dirname + "/" + "style.css");
-//   });
 
 app.get("/login", (req,res) => {
     
@@ -32,20 +29,33 @@ app.post("/register", (req,res) => {
     let data = fs.readFileSync(filename);
     let db = JSON.parse(data);
     let email = req.body.email;
+    password = req.body.email;
 
     db.users.forEach(user => {
         if(user.email === email) {
             res.send("Email already exists");
+            res.end()
         }
         if (req.body.password != req.body.cnfpass) {
-            res.send("password doesnt match")
+            res.send("password doesnt match");
+            res.end();
         }
     })
+
+            
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+            user.password= hash;
+            user.cnfpass=hash;
+
              db.users.push(user);
 
             fs.writeFile(filename,JSON.stringify(db,null,'\t'),(err) => {
              if(err) console.log(err);
              res.send('User added!');
+
+
+            
     })
     })
     
@@ -58,16 +68,19 @@ app.post("/register", (req,res) => {
         let db = JSON.parse(data);
         let password= req.body.password;
         let email= req.body.email;
+        
 
         db.users.forEach(user => {
-            if ( user.email === email && user.password === password ) {
-                // res.sendFile(path.join(__dirname + '/index.html'));
-                res.send("Successful login");
-            }
-            else {
-                res.send("Wrong credentials!!");
-            }
+            // if ( user.email === email && user.password === password ) {
+            //     // res.sendFile(path.join(__dirname + '/index.html'));
+            //     res.send("Successful login");
+            // }
+            let hash = user.password;
+            bcrypt.compareSync(password, hash); // true
+            res.send("logged in successfulyy");
         })
+
+        res.send("wrong credentials");
 
 
     })
